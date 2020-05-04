16317f356dbeed00a4057358ed443597735fc70f
Lucas Matheus Santos Cardoso <lucasmatheussc97@gmail.com>
2020-05-04 15:20:22 -0300

    Updates

---
+++ precache-manifest.9060367c1bb216a37b74c9e0b70acfde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d5ae9c8fb53e242254097afe4cb4d2",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "1722bc58faa726e0b021",
+    "url": "/myprofile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "712dafa129768df0cf04",
+    "url": "/myprofile/static/js/2.6ade7fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.6ade7fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1722bc58faa726e0b021",
+    "url": "/myprofile/static/js/main.4e3be2b6.chunk.js"
+  },
+  {
+    "revision": "3fef66b93c05b88c4977",
+    "url": "/myprofile/static/js/runtime-main.d2124533.js"
+  }
+]);
